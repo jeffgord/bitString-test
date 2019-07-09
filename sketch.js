b9cd510bb50c893658ecd3d2f0ed74f2f07711f8
mkeren22 <51344504+mkeren22@users.noreply.github.com>
2019-07-09 15:30:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
+/* 
+bitString Prototype
+Jeff Gordon and Maya Keren
+Advisors: Aatish Bhatia and Dan Trueman
+7/9/19
+*/
 
 function sketch(parent) {
   return function( p )
-    { // p could be any variable name
+    {
 
     let osc; // simple oscillator
     let amp = 0.5; // start with amp of 0.5
-    let currentFreq;
-    let prevFreq;
+    let currentFreq; // current frequency
+    let prevFreq; // old frequency
     let canvas; // canvas
+    let mute = true; // toggles sound
 
     p.setup = function() {
       canvas = p.createCanvas(400, 400);
@@ -16,18 +23,34 @@ function sketch(parent) {
       currentFreq = parent.data.freq;
       prevFreq = currentFreq;
 
+      // create osc
       osc = new p5.Oscillator();
-      osc.start();
       osc.setType('sine');
       osc.freq(currentFreq);
       osc.amp(amp);
-      console.log(osc);
+    };
 
+    // mute function
+    p.keyPressed = function() {
+      // keycode m val
+      if (p.keyCode === 77) {
+        console.log("pressed");
+        mute = !mute;
+        // toggle sound on and off
+        if (!mute) {
+          osc.start();
+        }
+        else {
+          osc.stop();
+        }
+      }
     };
 
     p.draw = function() {
       // change background color based on freq
       p.background(p.map(currentFreq, 200, 600, 0, 255));
+
+      // set frequency to user input
       currentFreq = parseFloat(parent.data.freq);
       osc.freq(currentFreq);
     };
