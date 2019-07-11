0b7f2cd2ae6fa82f3fc4dadbebe23ccd5063f4d6
jeffgord <51277885+jeffgord@users.noreply.github.com>
2019-07-11 18:52:58 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,36 +18,50 @@ function sketch(parent) {
 
     p.setup = function() {
 
-    	// set current fundamental frequency
-     	currentFreq = parseFloat(parent.data.fundamental);
-
-     	// canvas
+     	// sketch settings
      	canvas = p.createCanvas(400, 400);
      	canvas.parent(parent.$el);
-     	p.background(p.map(currentFreq, 200, 600, 0, 255));
+      p.noStroke();
+      p.fill(127, 212, 195); // rects are green
 
       	// initialize oscs, put in osc array, and populate amp array
       	for (let i = 0; i < numOscs; i++) {
         	ampArray[i] = parseFloat(parent.data.harmData[i].amp);
-
         	let osc = new p5.Oscillator();
         	osc.setType('sine');
         	osc.freq(parseFloat(currentFreq * parent.data.harmData[i].fMult));
         	osc.amp(ampArray[i]);
         	oscArray.push(osc);
         }
+
+        let dummy = 0;
+
+        p.initialize(parent.data, dummy);
     };
 
     // update the sketch only when data changes
     p.dataChanged = function(s, oldS) {
-    	currentFreq = parseFloat(s.fundamental);
+      p.initialize(s, oldS);
+    }
 
-    	for (let i = 0; i < numOscs; i++) {
-    		oscArray[i].amp(parseFloat(s.harmData[i].amp));
-    		oscArray[i].freq(currentFreq * s.harmData[i].fMult);
-    	}
+    p.initialize = function(s, oldS) {
+      // refresh window
+      p.background(255);
 
-    	p.background(p.map(currentFreq, 200, 600, 0, 255));
+      // set current fundamental frequency
+      currentFreq = parseFloat(s.fundamental);
+
+      for (let i = 0; i < numOscs; i++) {
+        let newAmp = parseFloat(s.harmData[i].amp);
+        let newMult = parseFloat(s.harmData[i].fMult);
+
+        oscArray[i].amp(newAmp);
+        oscArray[i].freq(currentFreq * newMult);
+
+        var x = p.map(currentFreq * newMult, 0, 10000, 0, canvas.width);
+        var y = p.map(newAmp, 0, 1, 0, canvas.height);
+        p.rect(x, canvas.height-y, 10, y, 20, 20, 0, 0);
+      }
     }
 
     // mute function
