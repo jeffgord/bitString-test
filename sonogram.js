4f2d581fabc56cbc5e0383ee80864c4cd57b08d9
jeffgord <51277885+jeffgord@users.noreply.github.com>
2019-08-14 15:49:56 -0400

    Add files via upload

--- sonogram.js
+++ sonogram.js
@@ -9,17 +9,18 @@ function sketch(parent) {
       canvas = p.createCanvas(400, 400);
       canvas.parent(parent.$el);
       p.noStroke();
+      p.colorMode("rgb", 1);
     }
 
     p.draw = function() {
       ampArray = fft.analyze();
+      ampArray.length = p.ceil(8000 * 1024 / 22000); // best guess for upper frequency bound of 8,000. p5 documentation doesn't describe exact highest frequency of the fft :(
       ampArray.reverse();
 
       if(!parent.data.mute) {
       
         for (let i = 0; i < ampArray.length; i++) {
-          if (ampArray[i] != 0) console.log(ampArray[i]);
-          let yPos = p.map(i, 0, 1024, 0, canvas.height);
+          let yPos = p.map(i, 0, ampArray.length, 0, canvas.height);
           p.colorize(ampArray[i]);
           p.rect(t, yPos, 1, canvas.height / ampArray.length);
         }
@@ -30,21 +31,40 @@ function sketch(parent) {
 
     // maps color of rect along the color spectrum depending on amplitude
     p.colorize = function(amp) {
-      if (amp >= 0 && amp < 51) {
-        p.fill (255, amp, 0);
+
+      // log scale and avoiding unecessary calculations
+      if (amp != 0) {
+        amp = p.map(amp, 0, 255, 0, 6);
+        //amp = p.map(Math.log(amp + 1), 0, Math.log(256), 0, 6);
       }
-      
-      if (amp >= 51 && amp < 102) {
-        p.fill (255 - amp, 255, 0);
+
+      let colorMap;
+
+      if (amp >= 5) {
+        colorMap = p.map(amp, 5, 6, 0, 1);
+        p.fill(1, 1 - colorMap, 0);
       }
-      
-      if (amp >= 153 && amp < 204) {
-        p.fill (0, 255, amp);
+      else if (amp >= 4) {
+        colorMap = p.map(amp, 4, 5, 0, 1);
+        p.fill(colorMap, 1, 0);
       }
-      
-      if (amp >= 204 && amp <= 255) {
-        p.fill (0, 255 - amp, 255);
+      else if (amp >= 3) {
+        colorMap = p.map(amp, 3, 4, 0, 1);
+        p.fill(0, 1, 1 - colorMap);
       }
+      else if (amp >= 2) {
+        colorMap = p.map(amp, 2, 3, 0, 1);
+        p.fill(0, colorMap, 1);
+      }
+      else if (amp >= 1) {
+        colorMap = p.map(amp, 1, 2, 0, 1);
+        p.fill(1 - colorMap, 0, 1);
+      }
+      else {
+        colorMap = amp;
+        p.fill(1, 1 - colorMap, 1);
+      }
+
     }
   }
 }
